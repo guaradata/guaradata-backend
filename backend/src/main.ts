2c9a82a232204075f2e682ac28569657022572e5
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2024-05-10 11:24:41 -0300

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -11,6 +16,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
